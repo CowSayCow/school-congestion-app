b5e29c9edd9168a476980d1e8e3fee366c52c48f
CowSayCow <tomatoketilyappu@gmail.com>
2025-07-18 16:43:13 +0900

    URL変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import  SchoolCongestionApp from "@/components/SchoolCongestionApp"
 
 export default async function HomePage() {
-  const res = await fetch("http://localhost:8000/api/congestion", {
+  const res = await fetch("https://backendserver-rvp3.onrender.com/api/congestion", {
     cache: "no-store",
   })
 
